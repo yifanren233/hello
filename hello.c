51ac73124b08ee569f5a55747144142ed9da6f3a
yifanren233 <42580774+yifanren233@users.noreply.github.com>
2019-01-17 11:27:29 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+/**
+*  Author:Yifanren
+*  Date: 2018/1/17
+*
+*  A simple hello world program in C
+*
+*/
+#include<stdlib.h>
+#include<stdio.h>
+int main(int argc, char **argv) {
+printf("Yifan Ren\n");
+return 0;
+}
